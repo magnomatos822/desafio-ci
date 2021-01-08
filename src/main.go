2e15464500e67b941841a070a815a0a6a2140604
Magno Matos <magnomtos822@gmail.com>
2021-01-08 11:43:31 -0300

    init repositori

---
+++ src/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "modulo/somar"
+
+func main() {
+	var (
+		numero1 = 5
+		numero2 = 5
+	)
+	somar.Somar(numero1, numero2)
+}
---
+++ src/somar/somar.go
@@ -0,0 +1,9 @@
+package somar
+
+import "fmt"
+
+//Fução de soma de dois números
+func Somar(numero1 int, numero2 int) {
+	soma := numero1 + numero2
+	fmt.Println(soma)
+}
